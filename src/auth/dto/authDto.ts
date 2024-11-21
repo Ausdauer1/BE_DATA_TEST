9f11568b181a816f557c4f6964b287ff60e95390
Ausdauer1 <steady051@naver.com>
2024-11-21 10:53:20 +0900

    change authDto

--- src/auth/dto/authDto.ts
+++ src/auth/dto/authDto.ts
@@ -1,26 +1,20 @@
 import { IsEmail, IsOptional, IsString ,Length, MinDate, MinLength} from "class-validator";
 export namespace AuthDto {
     export class SignUp {
-      // @IsString()
-      // @Length(2, 12)
-      // id: string;
 
-      @IsString()
-      // @MinLength(2, {
-      //   message: "닉네임의 길이가 짧아용ㅠㅠ"
-      // })
+      @IsString({ message: "문자열로 작성해주세요" })
+      @MinLength(2, { message: "닉네임은 최소 2자 ~ 최대 12자로 설정해주세요." })
       nickname: string;
 
-      @IsString()
-      @Length(8, 20, {
-        message: "길이가 8이상 20미만"
-      })
+      @IsString({ message: "문자열로 작성해주세요" })
+      @Length(8, 20, { message: "비밀번호는 최소 2자 ~ 최대 12자로 설정해주세요" })
       password: string;
 
-      @IsEmail()
+      @IsEmail({}, { message: "비밀번호는 최소 2자 ~ 최대 12자로 설정해주세요" })
       email: string;
       
       @IsString()
+      @IsOptional()
       type: string;
   
     }
@@ -31,9 +25,9 @@ export namespace AuthDto {
       // id: string;
 
       @IsString()
-      // @MinLength(2, {
-      //   message: "닉네임의 길이가 짧아용ㅠㅠ"
-      // })
+      @MinLength(2, {
+        message: "닉네임의 길이가 짧아용ㅠㅠ"
+      })
       nickname: string;
 
       @IsEmail()
