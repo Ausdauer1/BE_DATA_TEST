ef7800d18ddc3d7262cb62b980170c9dbb145927
Ausdauer1 <steady051@naver.com>
2024-11-25 10:42:39 +0900

    prettier 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,103 +8,103 @@ import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    private redisClient = createClient({ url: 'redis://:dangsan10@43.201.37.98:6379'});
+  private redisClient = createClient({
+    url: 'redis://:dangsan10@43.201.37.98:6379',
+  });
 
-    constructor(
-        @InjectRepository(USER)
-        private userRepository: Repository<USER>
-    ) {
-        this.redisClient.connect().catch(console.error);
+  constructor(
+    @InjectRepository(USER)
+    private userRepository: Repository<USER>,
+  ) {
+    this.redisClient.connect().catch(console.error);
+  }
+
+  async create(authDto: AuthDto.SignUp | AuthDto.signUpSocial) {
+    console.log(authDto);
+    const userEntity = this.userRepository.create(authDto);
+    console.log(userEntity);
+    if (userEntity.password) {
+      userEntity.password = await bcrypt.hash(userEntity.password, 10);
     }
+    const info = await this.userRepository.save(userEntity);
+    return info;
+  }
 
-    
+  // async findById(id: string) {
+  //     return await this.userRepository.findOne({
+  //         where: {
+  //             id: id
+  //         }
+  //     })
+  // }
 
-    async create(authDto: AuthDto.SignUp | AuthDto.signUpSocial) {
-        console.log(authDto)
-        const userEntity = this.userRepository.create(authDto)
-        console.log(userEntity)
-        if (userEntity.password) {
-            userEntity.password = await bcrypt.hash(userEntity.password, 10)
+  async findByEmail(email: string) {
+    return await this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+  }
+
+  async findByNickname(nickname: string) {
+    return await this.userRepository.findOne({
+      where: {
+        nickname,
+      },
+    });
+  }
+
+  async login(email: string, password: string, req): Promise<any> {
+    // 여기에서 실제 사용자 인증 로직을 처리합니다 (DB 조회 또는 하드코딩 예시)
+
+    const user = await this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    let passMatch: boolean;
+    console.log(user);
+    if (!user) return false;
+    if (user.type == 'email')
+      passMatch = await bcrypt.compare(password, user.password);
+    console.log(passMatch);
+    if (user.type === 'google' || (email === user.email && passMatch)) {
+      const keyArr = await this.redisClient.keys('*');
+      const deletePromises = keyArr.map(async (e) => {
+        console.log(e);
+        const valueString = await this.redisClient.get(`${e}`);
+        const value: object = JSON.parse(valueString);
+        if (value['user'] === email) {
+          await this.redisClient.del(`${e}`);
+          console.log('delete');
         }
-        const info = await this.userRepository.save(userEntity)
-        return info
+      });
+
+      await Promise.all(deletePromises);
+
+      req.session.user = user.email;
+      await req.session.save();
+      return {
+        sid: req.sessionID,
+        id: user.id,
+        email: user.email,
+        nickname: user.nickname,
+      };
     }
 
-    // async findById(id: string) {
-    //     return await this.userRepository.findOne({
-    //         where: {
-    //             id: id
-    //         }
-    //     })
-    // }
+    return false;
+  }
 
-    async findByEmail(email: string) {
-        return await this.userRepository.findOne({
-            where: {
-                email
-            }
-        })
+  async login2(id: string, password: string, req): Promise<boolean> {
+    // 여기에서 실제 사용자 인증 로직을 처리합니다 (DB 조회 또는 하드코딩 예시)
+    const validUser = { id: 'user1', password: 'pass1' }; // 예시 사용자
+
+    if (id === validUser.id && password === validUser.password) {
+      req.session.user = { username: validUser.id }; // 세션에 사용자 정보 저장
+      return true;
     }
 
-    async findByNickname(nickname: string) {
-        return await this.userRepository.findOne({
-            where: {
-                nickname
-            }
-        })
-    }
-
-    async login(email: string, password: string, req): Promise<any> {
-        // 여기에서 실제 사용자 인증 로직을 처리합니다 (DB 조회 또는 하드코딩 예시) 
-        
-        const user = await this.userRepository.findOne({
-            where: {
-                email
-            }
-        })
-
-        let passMatch: boolean;
-        console.log(user)
-        if (!user) return false
-        if (user.type == "email") passMatch = await bcrypt.compare(password, user.password)
-            console.log(passMatch)
-        if (user.type === "google" || (email === user.email && passMatch)) {
-            
-            const keyArr = await this.redisClient.keys('*')
-            const deletePromises = keyArr.map(async e => {
-                console.log(e)
-                const valueString = await this.redisClient.get(`${e}`)
-                const value: object = JSON.parse(valueString)
-                if (value["user"] === email) {
-                    await this.redisClient.del(`${e}`)
-                    console.log("delete")
-                }
-            })
-
-            await Promise.all(deletePromises);
-
-            req.session.user = user.email;
-            await req.session.save();
-            return {
-                sid: req.sessionID,
-                id: user.id,
-                email: user.email,
-                nickname: user.nickname
-            }
-        }
-    
-        return false;
-    }
-
-    async login2(id: string, password: string, req): Promise<boolean> {
-        // 여기에서 실제 사용자 인증 로직을 처리합니다 (DB 조회 또는 하드코딩 예시)
-        const validUser = { id: 'user1', password: 'pass1' }; // 예시 사용자
-        
-        if (id === validUser.id && password === validUser.password) {
-            req.session.user = { username: validUser.id }; // 세션에 사용자 정보 저장
-            return true;
-        }
-    
-        return false;
-    }
+    return false;
+  }
 }
--- src/auth/dto/authDto.ts
+++ src/auth/dto/authDto.ts
@@ -1,64 +1,56 @@
-import { IsEmail, IsOptional, IsString ,Length, MinDate, MinLength} from "class-validator";
+import { IsEmail, IsOptional, IsString, Length, MinDate, MinLength } from 'class-validator';
 export namespace AuthDto {
-    export class SignUp {
+  export class SignUp {
+    @IsString({ message: '문자열로 작성해주세요' })
+    @Length(2, 12, { message: '닉네임은 최소 2자 ~ 최대 12자로 설정해주세요' })
+    nickname: string;
 
-      @IsString({ message: "문자열로 작성해주세요" })
-      @MinLength(2, { message: "닉네임은 최소 2자 ~ 최대 12자로 설정해주세요." })
-      nickname: string;
+    @IsEmail({}, { message: '이메일 형식에 맞지 않습니다' })
+    email: string;
 
-      @IsString({ message: "문자열로 작성해주세요" })
-      @Length(8, 20, { message: "비밀번호는 최소 2자 ~ 최대 12자로 설정해주세요" })
-      password: string;
+    @IsString({ message: '문자열로 작성해주세요' })
+    @Length(8, 20, { message: '비밀번호는 최소 8자 ~ 최대 20자로 설정해주세요' })
+    password: string;
 
-      @IsEmail({}, { message: "비밀번호는 최소 2자 ~ 최대 12자로 설정해주세요" })
-      email: string;
-      
-      @IsString()
-      @IsOptional()
-      type: string;
-  
-    }
-
-    export class signUpSocial {
-      // @IsString()
-      // @Length(2, 12)
-      // id: string;
-
-      @IsString()
-      @MinLength(2, {
-        message: "닉네임의 길이가 짧아용ㅠㅠ"
-      })
-      nickname: string;
-
-      @IsEmail()
-      email: string;
-
-      @IsString()
-      type: string;
-  
-    }
-  
-    export class SignIn {
-      @IsEmail()
-      email: string;
-  
-      @IsOptional()
-      @IsString()
-      @Length(8, 20)
-      password: string;
-    }
-
-    export class signInSocial {
-      // @IsEmail()
-      email: string;
-  
-      // @IsString()
-      // @Length(8, 20)
-      password: string;
-    }
-
-    export class checkEmail {
-      @IsEmail()
-      email: string;
-    }
+    @IsString({ message: '문자열로 작성해주세요' })
+    @IsOptional()
+    type: string;
   }
+
+  export class signUpSocial {
+    @IsString({ message: '문자열로 작성해주세요' })
+    @Length(2, 12, { message: '닉네임은 최소 2자 ~ 최대 12자로 설정해주세요' })
+    nickname: string;
+
+    @IsEmail({}, { message: '이메일 형식에 맞지 않습니다' })
+    email: string;
+
+    @IsString({ message: '문자열로 작성해주세요' })
+    @IsOptional()
+    type: string;
+  }
+
+  export class SignIn {
+    @IsEmail({}, { message: '이메일 형식에 맞지 않습니다' })
+    email: string;
+
+    @IsOptional()
+    @IsString({ message: '문자열로 작성해주세요' })
+    @Length(8, 20, { message: '비밀번호는 최소 8자 ~ 최대 20자로 설정해주세요' })
+    password: string;
+  }
+
+  export class signInSocial {
+    @IsEmail({}, { message: '이메일 형식에 맞지 않습니다' })
+    email: string;
+
+    // @IsString()
+    // @Length(8, 20)
+    password: string;
+  }
+
+  export class checkEmail {
+    @IsEmail({}, { message: '이메일 형식에 맞지 않습니다' })
+    email: string;
+  }
+}
