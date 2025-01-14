c638adbab65d88f8417d93aa4ae630506c5c0d6d
Ausdauer1 <steady051@naver.com>
2025-01-14 18:22:25 +0900

    change origin

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,17 @@ import { setupSwagger } from 'src/util/swagger';
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
-  app.enableCors({credentials: true});
+  app.enableCors({
+    origin: [
+      'https://k-baseball.store:3000', 
+      'http://localhost:3000',
+      'http://localhost:3002'
+    ],
+    credentials: true
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   setupSwagger(app);
+
   await app.listen(3000);
 
 }
