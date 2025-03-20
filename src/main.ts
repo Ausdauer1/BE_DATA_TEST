34ea5b3a7e4c1744e063fa97d3bc8c8b330dac3e
Ausdauer1 <steady051@naver.com>
2025-03-20 11:54:24 +0900

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:3000",
     credentials: true
   });
   app.use(cookieParser())
